5349f8edbc3f26e1677d776c4164c58b62096a2f
Toby-SeoKyeong-Kim <seop0504@gmail.com>
2021-09-13 11:59:06 +0900

    animation added

--- Source/ETNZ_VRtist/ETNZ_VRtist.Build.cs
+++ Source/ETNZ_VRtist/ETNZ_VRtist.Build.cs
@@ -8,7 +8,7 @@ public class ETNZ_VRtist : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","UMG", "AudioMixer", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","UMG", "AudioMixer", "HeadMountedDisplay"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
